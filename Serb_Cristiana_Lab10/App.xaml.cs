63edc676969d2a09271d097ffc2367d6072b3076
CRISTIANA-MARIA ȘERB <cristiana.serb@stud.ubbcluj.ro>
2020-12-09 18:57:12 +0200

    Add project files.

---
+++ Serb_Cristiana_Lab10/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Serb_Cristiana_Lab10.Data;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Serb_Cristiana_Lab10
+{
+    public partial class App : Application
+    {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                    LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
